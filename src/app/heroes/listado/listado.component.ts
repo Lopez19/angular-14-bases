a1ab2885bf586dc7595722daf90a77fcaa525757
Lopez19 <horaciolopm_2001@hotmail.com>
2022-11-12 18:37:13 -0500

    Crear Componente automaticamente

---
+++ src/app/heroes/listado/listado.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-listado',
+  templateUrl: './listado.component.html',
+})
+export class ListadoComponent {
+  constructor() {
+    console.log('constructor');
+  }
+}
