a163d4d8f77c77e0d122b3bed6a0ec81b74650ae
Lopez19 <horaciolopm_2001@hotmail.com>
2022-11-12 20:38:36 -0500

    Modulos primer contacto

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { ContadorComponent } from './contador/contador.component';
-import { HeroeComponent } from './heroes/heroe/heroe.component';
-import { ListadoComponent } from './heroes/listado/listado.component';
+import { HeroesModule } from './heroes/heroes.module';
+import { ContadoresModule } from './contadores/contadores.module';
 
 @NgModule({
-  declarations: [AppComponent, ContadorComponent, HeroeComponent, ListadoComponent],
-  imports: [BrowserModule],
+  declarations: [AppComponent],
+  imports: [BrowserModule, HeroesModule, ContadoresModule],
   providers: [],
   bootstrap: [AppComponent],
 })
